60cefcdf099bd30ce808ad817e8983d3981ca4a6
maliniram2301 <53600726+maliniram2301@users.noreply.github.com>
2019-08-06 11:17:17 +0530

    Create positivenegative.py

---
+++ positivenegative.py
@@ -0,0 +1,9 @@
+v=int(input("enter a n numbers"))
+if (n>=0):
+    print("n is positive")
+elif (n<0):
+    print("n is negative")=int(input("enter a n numbers"))
+if (n>=0):
+    print("n is positive")
+elif (n<0):
+    print("n is negative")
