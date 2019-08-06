60970ac833dc50e29a117dc0895c727e4793bca1
maliniram2301 <53600726+maliniram2301@users.noreply.github.com>
2019-08-06 11:14:56 +0530

    Create greatest3 numbers.py

---
+++ greatest3 numbers.py
@@ -0,0 +1,10 @@
+a=int(input("input a value:"))
+b=int(input("input b value:"))
+c=int(input("input c value:"))
+if (a>b and a>c):
+    print("a is greater")
+elif (b>a and b>c):
+    print("b is greater")
+else:
+    print("c is greater")
+
