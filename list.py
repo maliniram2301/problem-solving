1f93bbba0a5e33ab686b9782a768ec1dfb4047c4
maliniram2301 <53600726+maliniram2301@users.noreply.github.com>
2019-08-09 14:22:31 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+list1=[15,8,6,11,16]
+n=int(input("enter a number"))
+if min(list1)>n:
+    print("yes all list values are greater than %i ." %n)
+else:
+    print("no all list values are not greater than %i ." %n)
