7d2c8a50899caad3c16d6537278a946806cf2500
maliniram2301 <53600726+maliniram2301@users.noreply.github.com>
2019-08-13 14:18:44 +0530

    Create sortorder.py

---
+++ sortorder.py
@@ -0,0 +1,7 @@
+items = input("enter sequence of words")
+words = [word for word in items.split(",")]
+print(",".join(sorted(list(set(words)))))items = input("enter sequence of words")
+words = [word for word in items.split(",")]
+print(",".join(sorted(list(set(words)))))items = input("enter sequence of words")
+words = [word for word in items.split(",")]
+print(",".join(sorted(list(set(words)))))
